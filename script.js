872371192710d3f807ec368398b76046fd2ad04f
ayindesadiq01 <ayindesadiq01@gmail.com>
2023-08-05 07:32:28 +0100

    new changes

--- script.js
+++ script.js
@@ -35,44 +35,45 @@ const sectionObserver = new IntersectionObserver(headerFtn, {
 sectionObserver.observe(header);
 
 //REVEALING SECTIONS ON SCROLL
-// const sections = document.querySelectorAll('.section')
+const sections = document.querySelectorAll('.section')
 
-// const scrollFtn= function(entries, observe) {
-//  const[entry] = entries;
-//  console.log(entry)
+const scrollFtn= function(entries, observe) {
+ const[entry] = entries;
+ console.log(entry)
 
-//  if(!entry.isIntersecting) return;
+ if(!entry.isIntersecting) return;
  
-//   entry.target.classList.remove('section-hidden');
+  entry.target.classList.remove('section-hidden');
 
-//   // To Unobserve
-//   elScroll.unobserve(entry.target)
-// }
+  // To Unobserve
+  elScroll.unobserve(entry.target)
+}
 
-// const elScroll = new IntersectionObserver(scrollFtn, {
-//  root: null,
-//  threshold: 0.15,
-// });
+const elScroll = new IntersectionObserver(scrollFtn, {
+ root: null,
+ threshold: 0.15,
+});
 
-// sections.forEach(section => {
-//  //To Observe the sections
-//  elScroll.observe(section);
+sections.forEach(section => {
+ //To Observe the sections
+ elScroll.observe(section);
 
-//  //To hide the sections
-//  section.classList.add('section-hidden');
-// })
+ //To hide the sections
+ section.classList.add('section-hidden');
+})
 
-// // WORKING ON DOTS
-// const dots = document.querySelector('.dots');
-// dots.addEventListener('click', (e) => {
-//  // console.log(e.target.contain('.dot'))
-// })
+// WORKING ON DOTS
+const dots = document.querySelector('.dots');
+dots.addEventListener('click', (e) => {
+ // console.log(e.target.contain('.dot'))
+})
 
 // WORKING ON SLIDER IN COMMENT SECTION
 const btnLeft = document.querySelector('.btn--left');
 const btnRight = document.querySelector('.btn--right');
 const slides = document.querySelectorAll('.slide');
 const slider = document.querySelector('.slider');
+const btnAll = document.querySelectorAll('.dot');
 
 // slider.style.transform = "scale(.5)"
 // slider.style.overflow = 'visible'
@@ -80,9 +81,25 @@ const slider = document.querySelector('.slider');
 let culSlide = 0;
 const maxSlide = slides.length;
 
-slides.forEach((sl, i) => {
- sl.style.transform = `translateX(${100 * i}%)`
-})
+// slides.forEach((sl, i) => {
+//  sl.style.transform = `translateX(${100 * i}%)`
+// })
+
+const gotoSlide = function(slide) {
+  slides.forEach((sl, i) => {
+  sl.style.transform = `translateX(${100 * (i - slide)}%)`
+  })
+}
+
+gotoSlide(0)
+
+const activeDots = function(slide) {
+  btnAll.forEach(btn => {
+    btn.classList.remove('active-dot')
+  })
+
+  document.querySelector(`.dot[data-slide="${slide}"]`).classList.add('active-dot');
+}
 
 //BUTTON RIGHT
 btnRight.addEventListener('click', () => {
@@ -93,53 +110,80 @@ if(culSlide === maxSlide -1) {
  culSlide++;
 }
 
-slides.forEach((sl, i) => {
- sl.style.transform = `translateX(${100 * (i - culSlide)}%)`
-})
+gotoSlide(culSlide);
+activeDots(culSlide);
+
 })
 
 // BUTTON LEFT 
 btnLeft.addEventListener('click', () => {
- console.log(culSlide, maxSlide)
- if(culSlide === 0) {
-  culSlide = maxSlide -1
- } else {
-  culSlide--;
- }
- slides.forEach((sl, i) => {
- sl.style.transform = `translateX(${100 * (i - culSlide)}%)`
-})
+  console.log(culSlide, maxSlide)
+  if(culSlide === 0) {
+    culSlide = maxSlide -1
+  } else {
+    culSlide--;
+  }
+  
+  gotoSlide(culSlide);
+  activeDots(culSlide);
+
 })
 
 // USING THE LEFT AND RIGHT ARROW FOR SLIDE
 //RIGHT KEY
 document.addEventListener('keydown', (e) => {
- if (e.key === 'ArrowRight') {
-  console.log('correct')
-  if(culSlide === maxSlide -1) {
- culSlide = 0;
- 
-} else {
- culSlide++;
-}
-
-slides.forEach((sl, i) => {
- sl.style.transform = `translateX(${100 * (i - culSlide)}%)`
-})
- }
+  if (e.key === 'ArrowRight') {
+    console.log('correct')
+    if(culSlide === maxSlide -1) {
+      culSlide = 0;
+    } else {
+      culSlide++;
+    }
+    
+    gotoSlide(culSlide)
+    activeDots(culSlide)
+  }
 })
 
 //LEFT KEY
 document.addEventListener('keydown', (e) => {
- if (e.key === 'ArrowLeft') {
-   if(culSlide === 0) {
-  culSlide = maxSlide -1
- } else {
-  culSlide--;
- }
+  if (e.key === 'ArrowLeft') {
+    if(culSlide === 0) {
+      culSlide = maxSlide -1
+    } else {
+      culSlide--;
+    }
+  
+  gotoSlide(culSlide)}
 
- slides.forEach((sl, i) => {
- sl.style.transform = `translateX(${100 * (i - culSlide)}%)`
+
 })
- }
+
+// WORKING WITH THE DOTS BUTTON
+const dotContainer = document.querySelector('.dots');
+
+dotContainer.addEventListener('click', e =>{
+  const clicked = e.target.classList.contains('dot');
+
+  if(clicked) {
+    
+    const slide = e.target.dataset.slide;
+  
+    gotoSlide(slide);
+
+  }
+})
+
+
+// IMPLEMENTING SMOOTH SCROLLING
+document.querySelector('.link-lists').addEventListener('click', e => {
+  e.preventDefault();
+
+  if(e.target.classList.contains('link-list')) {
+
+    const id = e.target.getAttribute('href')
+    console.log(id);
+
+    document.querySelector(id).scrollIntoView({behavior: 'smooth'});
+  }
 })
